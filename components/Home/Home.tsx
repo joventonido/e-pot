5108fb8e4457bec2dcf62c7c8ab027a8ce8db1c9
Joven Tonido <tonidojoventino25@gmail.com>
2025-10-08 00:05:57 +0800

    updating home component

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-// import Hero from "./Hero";
-// import About from "./About";
+import Hero from "./Hero";
+import About from "./About";
 
 const Home = () => {
   return (
-    <div className="overflow-hidden h-[2000px]">
-      {/* <Hero />
-      <About /> */}
+    <div className="overflow-hidden">
+      <Hero />
+      <About />
     </div>
   );
 };
