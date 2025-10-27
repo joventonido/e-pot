6f34efc18583512d63b11938b9ff1f94329e262a
Joven Tonido <tonidojoventino25@gmail.com>
2025-10-27 17:52:49 +0800

    adding sample dynamic route service id v3

--- app/service/[id]/page.tsx
+++ app/service/[id]/page.tsx
@@ -7,7 +7,7 @@ type ServiceInfoProps = {
 };
 
 const ServiceInfo = async ({ params }: ServiceInfoProps) => {
-  const { id } = params;
+  const id = (await params).id;
   return (
     <div className="relative w-full h-screen flex justify-center flex-col">
       <div className="w-[90%] md:w-[80%] mx-auto items-center grid grid-cols-1 xl:grid-cols-2 gap-10">
