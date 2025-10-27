21206f11705aa2b376d5d400f64a4bdf7851a55c
Joven Tonido <tonidojoventino25@gmail.com>
2025-10-27 18:59:19 +0800

    adding sample dynamic route service id v5

--- app/services/[serviceId]/page.tsx
+++ app/services/[serviceId]/page.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 
-type PageProps = {
-  params: { id: string };
-};
+// type PageProps = {
+//   params: { id: string };
+// };
 
-const ServiceInfo = async ({ params }: PageProps) => {
-  const id = (await params).id;
+const ServiceInfo = async ({
+  params,
+}: {
+  params: Promise<{ serviceId: string }>;
+}) => {
+  const serviceId = (await params).serviceId;
   return (
     <div className="relative w-full h-screen flex justify-center flex-col">
       <div className="w-[90%] md:w-[80%] mx-auto items-center grid grid-cols-1 xl:grid-cols-2 gap-10">
-        <div>Service ID: {id}</div>
+        <div>Service ID: {serviceId}</div>
       </div>
     </div>
   );
